7aa696196fc7ef6656f7aa5059aee6eabfed6f26
Aleksey Konotop <alekseykonotop@gmail.com>
2019-12-19 16:14:31 +0300

    Added serializers in stores app and view-functions in views for download and update price from pertner, get current status of pertner and change it, get actual orders to partners.

--- orders/stores/urls.py
+++ orders/stores/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import PartnerUpdate
+from .views import PartnerUpdate, PartnerState, PartnerOrders
 from rest_framework.authtoken.views import obtain_auth_token
 
 app_name = 'stores'
 
 urlpatterns = [
-    path('import-price/', PartnerUpdate.as_view()),
-    path('api-token-auth/', obtain_auth_token),
+    path('partner/update', PartnerUpdate.as_view(), name='partner-update'),
+    path('partner/state', PartnerState.as_view(), name='partner-state'),
+    path('partner/orders', PartnerOrders.as_view(), name='partner-orders'),
+    # path('api-token-auth/', obtain_auth_token),
 ]
