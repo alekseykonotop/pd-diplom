8fad0386d7c8f042ff8cdebc2fd90fccc6b256ef
Aleksey Konotop <alekseykonotop@gmail.com>
2019-12-29 21:26:47 +0300

    Minor edit in name of path in urlpatterns in stores.urls.

--- orders/stores/urls.py
+++ orders/stores/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('partner/orders', PartnerOrders.as_view(), name='partner-orders'),
     path('categories', CategoryView.as_view(), name='categories'),
     path('shops', ShopView.as_view(), name='shops'),
-    path('products', ProductInfoView.as_view(), name='shops'),
+    path('products', ProductInfoView.as_view(), name='products'),
     path('basket', BasketView.as_view(), name='basket'),
     path('order', OrderView.as_view(), name='order'),
 ]
